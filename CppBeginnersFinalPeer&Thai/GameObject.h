3d82c46bd18a6de1ce0df72b4de52d977bed8c0c
Tabolrin <93069446+Tabolrin@users.noreply.github.com>
2025-07-19 22:16:54 +0300

    minor change of access setting in gameObject

--- CppBeginnersFinalPeer&Thai/GameObject.h
+++ CppBeginnersFinalPeer&Thai/GameObject.h
@@ -6,34 +6,30 @@
 #include "Vector2.h"
 
 // Abstract base class for game objects
-class GameObject {
-private:
-    std::string tag;          // Tag to identify the game object
-    Vector2 worldLocation;    // 2D position in the game world
+class GameObject 
+{
+    private:
+        std::string tag;          
+        Vector2 worldLocation;    
 
-public:
-    // Constructor to initialize tag and worldLocation
-    GameObject(const std::string& tag, const Vector2& location)
-        : tag(tag), worldLocation(location) {
-    }
+    protected:
+        void setWorldLocation(const Vector2& newLocation) { worldLocation = newLocation; }
 
-    // Pure virtual function to make the class abstract
-    virtual void update() = 0;
+        void setTag(const std::string& newTag) { tag = newTag; }
 
-    // Virtual destructor for proper cleanup
-    virtual ~GameObject() {}
+    public:
+        GameObject(const std::string& tag, const Vector2& location) : tag(tag), worldLocation(location) {}
 
-    // Inline getter for tag
-    std::string getTag() const { return tag; }
+        virtual void update() = 0;
 
-    // Inline setter for tag (restricted to class scope or friends)
-    void setTag(const std::string& newTag) { tag = newTag; }
 
-    // Inline getter for worldLocation
-    Vector2 getWorldLocation() const { return worldLocation; }
+        virtual ~GameObject() {}
 
-    // Inline setter for worldLocation
-    void setWorldLocation(const Vector2& newLocation) { worldLocation = newLocation; }
+
+        std::string getTag() const { return tag; }
+
+
+        Vector2 getWorldLocation() const { return worldLocation; }
 };
 
 #endif
