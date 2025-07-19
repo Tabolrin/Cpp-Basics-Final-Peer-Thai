e2f08bc74b0ecb1761ab01c68acd2e853c241c8a
Tabolrin <93069446+Tabolrin@users.noreply.github.com>
2025-07-20 01:03:39 +0300

    vector2 hash support, unsorted map added

--- CppBeginnersFinalPeer&Thai/Map.h
+++ CppBeginnersFinalPeer&Thai/Map.h
@@ -1,14 +1,24 @@
 #pragma once
+#include <unordered_map>
+#include <string>
+#include "Vector2.h"
+#include "GameObject.h"
+using namespace std;
+
 enum class MapType
 {
 	MAP_LEVEL1 = 1,
 	MAP_LEVEL2 = 2,
 	MAP_LEVEL3 = 3,
-
 };
 
 class Map
 {
+	public:
+	MapType mapType;
 
+	unordered_map<Vector2, GameObject> mapData;
+
+	Map(MapType type) : mapType(type) {}
 };
 
--- CppBeginnersFinalPeer&Thai/Vector2.h
+++ CppBeginnersFinalPeer&Thai/Vector2.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <functional> // For std::hash
+
 template <typename T>
 struct Vector2
 {
@@ -15,15 +17,32 @@ struct Vector2
         ++y;
     }
 
-
     Vector2 operator+(const Vector2& other) const
     {
         return Vector2(x + other.x, y + other.y);
     }
 
-
     Vector2 operator-(const Vector2& other) const
     {
         return Vector2(x - other.x, y - other.y);
     }
+
+    // Equality operator
+    bool operator==(const Vector2& other) const
+    {
+        return x == other.x && y == other.y;
+    }
 };
+
+// Hash specialization for Vector2<T>
+namespace std 
+{
+    template <typename T>
+    struct hash<Vector2<T>>
+    {
+        std::size_t operator()(const Vector2<T>& v) const
+        {
+            return std::hash<T>()(v.x) ^ (std::hash<T>()(v.y) << 1);
+        }
+    };
+}
